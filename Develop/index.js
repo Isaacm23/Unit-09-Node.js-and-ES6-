18f0a26fbcf9147fdb259a5fe4f86bca92fb2976
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-05-29 22:02:31 -0700

    added test questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,46 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const appendFileAsync = util.promisify(fs.appendFile);
+const readFileAsync = util.promisify(fs.readFile);
+
+
 const questions = [
+    {
+        type: "input",
+        message: "What is the name of your repo?:",
+        name: "Repo Name"
+      },
+      {
+        type: "input",
+        message: "What is your github username?:",
+        name: "username"
+      },
+      {
+        type: "input",
+        message:"Write a discription of this repo:",
+        name: "confirm"
+      }
 
 ];
 
-function writeToFile(fileName, data) {
+function writeToFile(data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./Develop/README.md',data,err =>{
+            if (err){
+                reject(err);
+                return;
+            }
+            resolve({
+                ok:true,
+                message: "New file created"
+
+             });
+
+            });
+        });
 }
 
-function init() {
-
+function init(){
+    return inquirer.prompt(questions)
 }
-
-init();
